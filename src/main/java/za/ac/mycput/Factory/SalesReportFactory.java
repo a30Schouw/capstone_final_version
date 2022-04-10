55dd4a3a4ec49f5ead5f5935b96005b2eae7cee2
Marco0198 <81382003+Marco0198@users.noreply.github.com>
2022-04-10 22:17:30 +0200

    customer and  saleReport factory

--- src/main/java/za/ac/mycput/Factory/SalesReportFactory.java
+++ src/main/java/za/ac/mycput/Factory/SalesReportFactory.java
@@ -1,4 +1,9 @@
-package za.ac.mycput.Factory;
+
+/* Customer.java
+ Entity for the Customer
+ Author: Marco Mulondayi Tshimanga (219049505)
+ Date: 04 April 2022
+*/package za.ac.mycput.Factory;
 import za.ac.mycput.Entity.SalesReport;
 
 public class SalesReportFactory {
